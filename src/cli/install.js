b4137a8388b780545ca6ce9533d4133d2e3274dc
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-05-17 09:48:50 +0530

    feat: Refactor package installation logic to use tarball URL and improve error handling

--- src/cli/install.js
+++ src/cli/install.js
@@ -3,6 +3,16 @@ import { downloadPackage } from '../utils/downloadPackage.js';
 import { extractPackage, readPackageJson } from '../utils/fileSystem.js';
 import { fetchPackageInformation } from '../utils/fetchPackageInformation.js';
 import logger from '../utils/logger.js';
+import { config } from '../config.js';
+import axios from 'axios';
+//
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+import { config } from '../config.js';
+import logger from './logger.js';
 
 export const install = async (packageName, customVersion = null) => {
     try {
@@ -26,9 +36,25 @@ export const install = async (packageName, customVersion = null) => {
 
         const data = await fetchPackageInformation(packageName);
         const version = customVersion || data['dist-tags'].latest;
-        const filePath = await downloadPackage(packageName, version);
-        await extractPackage(packageName, filePath);
+        // const filePath = await downloadPackage(packageName, version);
+        // await extractPackage(packageName, filePath);
+
+        const tarballUrl = `${config.registry}${packageName}/-/${packageName}-${version}.tgz`;
+
+        const response = await axios({
+            url: tarballUrl,
+            responseType: 'stream',
+        });
+
+        await new Promise((resolve, reject) => {
+            response.data
+                .pipe(tar.x({ C: path.join(process.cwd(), 'node_modules', packageName), strip: 1 }))
+                .on('finish', resolve)
+                .on('error', reject);
+        });
+
         await addPackageToJson(packageName, version);
+        console.log('packageName', packageName);
         logger.success(`Package ${packageName}@${version} installed successfully.`);
     } catch (error) {
         logger.error(`Error while installing package: ${error}`);
