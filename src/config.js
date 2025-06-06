9afaeb649e0ec2a3649f4d7fa1e3af7d3b1288f8
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-06-06 22:11:07 +0530

    style: Standardize quotes in config.js and update backend API URL

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-import path from 'path';
+import path from "path";
 
 export const config = {
-    registry: 'https://registry.npmjs.org/',
-    cacheDirectory: '.flux-cache',
-    cachePath: path.join(process.cwd().slice(0, 2), '.flux-store'),
-    backendAPI: 'http://localhost:3000/api/v1',
+    registry: "https://registry.npmjs.org/",
+    cacheDirectory: ".flux-cache",
+    cachePath: path.join(process.cwd().slice(0, 2), ".flux-store"),
+    backendAPI: "http://apiflux.gautamsuthar.in/api/v1",
 };
