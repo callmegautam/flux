7a279dad502d4df1cd28b049b3ce422538a2e41b
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-09-08 17:15:32 +0530

    fix: Update backend API URL in config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ export const config = {
     registry: "https://registry.npmjs.org/",
     cacheDirectory: ".flux-cache",
     cachePath: path.join(process.cwd().slice(0, 2), ".flux-store"),
-    backendAPI: "http://apiflux.gautamsuthar.in/api/v1",
+    backendAPI: "http://api.gautamsuthar.in/flux/api/v1",
 };
